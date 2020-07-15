4b90964f8d075bad54176e1f3ad7d2e9da8bf989
karthicksb02 <68296603+karthicksb02@users.noreply.github.com>
2020-07-15 14:36:00 +0530

    Create varletconst.js

---
+++ varletconst.js
@@ -0,0 +1,55 @@
+// string
+var name = 'Aswin';
+// int
+var age = 28;
+// boolean
+var voice = true;
+// Array
+var languages = ['English', 'Tamil', 'German'];
+// objects
+var skills = {'name':'Jagadeesh',
+              'lang': ['Python','GO','Javascripts']
+             }
+
+var a = null;
+console.log(a);
+var b = '';
+console.log(b);
+
+//let
+{
+    let name = "vishnu";
+    // name = "Raj";
+    console.log("inside let : ",name);
+}
+console.log(name);
+
+// const
+
+const brand = 'Iphone';
+console.log(brand);
+// brand = 'MI';
+const computer_brands = ['Dell','Microsoft', 'Mac']
+console.log(computer_brands);
+computer_brands.push('HP');
+console.log(computer_brands);
+// computer_brands = ['Sony','toshiba']
+
+var x = 16 + 'Aswin';
+console.log(x);
+var y = "whirlpool" + 5 +6;
+console.log(y);
+
+var z1 = 14.56;
+var z2 = 14;
+
+console.log({z1,z2});
+
+var y1 = 45e3;
+var y2 = 78e-2;
+console.log({y1,y2});
+
+var a = 5;
+var b = 6;
+console.log(a == b);
+console.log(skills);
